0987cd1dd49237ef8281bc7c0b4d7e5b11e8a55e
kunilovsky <kunilovsky@gmail.com>
2022-05-16 15:37:23 +0500

    Footer stat

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer
  *
@@ -11,24 +12,37 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'progdev' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'progdev' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'progdev' ), 'progdev', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+<!-- Footer -->
+<footer>
+	<div class="container">
+		<hr>
+		<div class="row text-center">
+			<div class="col-lg-4">
+				ProgDev ® 2022 <br>
+				<p class="fw-light text-uppercase">
+					ООО "САПЛ" <br>
+					ИНН 7203523418
+				</p>
+
+			</div>
+			<div class="col-lg-4 fw-light">
+				<a href="" title="Политика конфиденциальности">Политика конфиденциальности</a>
+				<br>
+				<a href="" title="Реквизиты ООО САПЛ">Реквизиты компании 🇷🇺</a> <br>
+				<a href="assets/img/mem.png" alt="Гарольд" target="_blank">&#128760;</a>
+			</div>
+			<div class="col-lg-4 fw-light text-decoration-none">
+				<a href="https://wa.me/79617795111" title="Переход в чат WhatsApp">WhatsApp Business</a> <br>
+				<a href="tel:+79617795111" title="Осуществить звонок">+7(961)77-95-111</a> <br>
+				<a href="mailto:info@progdev.ru" title="Написать на почту">info@progdev.ru</a>
+			</div>
+		</div>
+	</div>
+</footer>
+<!-- /Footer -->
 
 <?php wp_footer(); ?>
 
 </body>
+
 </html>
